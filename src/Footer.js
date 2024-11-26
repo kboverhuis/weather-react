29c5bc94bb04b4815e58a0c2e945222fc957c2e8
kboverhuis <kboverhuis@gmail.com>
2024-11-25 21:18:23 -0600

    Removed li css and links on footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,18 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      Built by
-      <a href="https://github.com/kboverhuis" target="_blank">
-        Kristi Boverhuis
-      </a>
-      and is open-sourced
-      <a href="https://github.com/kboverhuis/forecast-app" target="_blank">
-        on GitHub
-      </a>
-      and hosted
-      <a href="https://forecast-project-app.netlify.app/" target="_blank">
-        on Netlify
-      </a>
+      Built by Kristi Boverhuis and is open-sourced on GitHub and hosted on Netlify
     </footer>
   );
 }
