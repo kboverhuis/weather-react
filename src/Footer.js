4478d71b39e6586f78b8b8e2e00f642d790e6c10
kboverhuis <kboverhuis@gmail.com>
2024-11-25 21:03:31 -0600

    Added initial content using react and css

---
+++ src/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      Built by
+      <a href="https://github.com/kboverhuis" target="_blank">
+        Kristi Boverhuis
+      </a>
+      and is open-sourced
+      <a href="https://github.com/kboverhuis/forecast-app" target="_blank">
+        on GitHub
+      </a>
+      and hosted
+      <a href="https://forecast-project-app.netlify.app/" target="_blank">
+        on Netlify
+      </a>
+    </footer>
+  );
+}
---
+++ src/Weather.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  const [city, setCity] = useState(" ");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    console.log(response.data);
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+      description: response.data.condition.description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "8908d7b1834oa44093b7b64af0t393f3";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        onChange={updateCity}
+      />
+      <input type="submit" value="Search" />
+    </form>
+    
+    
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperture)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}mph</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
