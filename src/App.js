f8d70c8d8b51ed29993fab68cb6bb4b700d103d2
kboverhuis <kboverhuis@gmail.com>
2024-11-30 16:59:59 -0600

    Added js files for forecast and info

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Weather App</h1>
-          <Weather />
+        <h1>Weather App</h1>
+        <Weather />
       </header>
       <Footer />
     </div>
