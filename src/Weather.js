6e3e5a44e9557ba7af61e5ab0c292e809c22264f
kboverhuis <kboverhuis@gmail.com>
2024-12-02 21:47:22 -0600

    Added files for forecast and icon animation and enhanced css

--- src/Weather.js
+++ src/Weather.js
@@ -1,62 +1,92 @@
 import React, { useState } from "react";
 import axios from "axios";
+import WeatherForecast from "./WeatherForecast";
+import WeatherInfo from "./WeatherInfo";
+import "./css/Weather.css";
 
-export default function Weather() {
-  const [city, setCity] = useState(" ");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
+export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
+  const [weather, setWeather] = useState({ ready: false});
 
   function displayWeather(response) {
-    console.log(response.data);
-    setLoaded(true);
     setWeather({
+      ready: true,
+      coordinates: response.data.coordinates,
       temperature: response.data.temperature.current,
       wind: response.data.wind.speed,
       humidity: response.data.temperature.humidity,
       icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
       description: response.data.condition.description,
+      city: response.data.city,
+      date: new Date(response.data.time * 1000),
     });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    let apiKey = "8908d7b1834oa44093b7b64af0t393f3";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
+    search();
+  }
+
+  function search() {
+        const apiKey = "8908d7b1834oa44093b7b64af0t393f3";
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(displayWeather);
   }
 
   function updateCity(event) {
     setCity(event.target.value);
   }
 
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Enter a city..."
-        onChange={updateCity}
-        className="Input"
-      />
-      <input type="submit" className="Search" value="Search" />
-    </form>
-  );
 
-  if (loaded) {
+  if (weather.ready) {
     return (
-      <div>
-        {form}
-        <ul>
-          <li>Temperature: {Math.round(weather.temperture)}°C</li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {weather.wind}mph</li>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
-        </ul>
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                onChange={updateCity}
+                className="form-control search-input"
+              />
+            </div>
+
+            <div className="col-3">
+              <input
+                type="submit"
+                className="search-btn w-100"
+                value="Search"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weather} />
+        <WeatherForecast
+          coordinates={weather.coordinates}
+          city={weather.city}
+        />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/122224-kristi-boverhuis"
+            target="blank"
+          >
+            Kristi Boverhuis
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/kboverhuis/weather-react" target="blank">
+            opened-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="https://musical-babka-12bce5.netlify.app/" target="blank">
+            hosted on Netlify
+          </a>
+        </footer>
       </div>
     );
   } else {
-    return form;
+    search();
+    return "Loading...";
   }
 }
